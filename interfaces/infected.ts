6a00b84be06e955dc192d851c7bf0f3f64841109
Zulfiqar Ali Qureshi <zulfiqarqureshi619@gmail.com>
2022-11-20 16:20:55 +0500

    add: tsc interfaces for survivors, inventory among other things

---
+++ interfaces/infected.ts
@@ -0,0 +1,5 @@
+export interface Infected {
+    name: string
+    confirmedByCount: number
+    confirmedByNames: Array<string>
+}
---
+++ interfaces/survivor.ts
@@ -0,0 +1,15 @@
+import {Item} from "../data_store";
+
+export interface Survivor {
+    name: string
+    age: number
+    gender: string
+    lastLocation: Array<number>
+    inventory: Array<Inventory>
+    infected?: boolean
+}
+
+export interface Inventory {
+    item: Item,
+    quantity: number
+}
